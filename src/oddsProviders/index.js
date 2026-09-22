0af82e5b3aa4b4530a24d419ab0aeb7b445f64e3
Jacek444 <Jakgreenbay@gmail.com>
2026-09-22 17:19:49 -0500

    wire the odds api into odds provider router

--- src/oddsProviders/index.js
+++ src/oddsProviders/index.js
@@ -1,3 +1,31 @@
-export function getOddsProvider() {
-  throw new Error('Odds provider router not implemented yet');
+import { config } from '../config.js';
+import { HttpError } from '../errors.js';
+import { theOddsApiProvider } from './theOddsApiProvider.js';
+
+const providers = {
+  theOddsApi: theOddsApiProvider
+};
+
+function isConfigured(name) {
+  const providerConfig = config.oddsProviders[name];
+
+  if (!providerConfig) {
+    return false;
+  }
+
+  return providerConfig.enabled && Boolean(providerConfig.apiKey);
+}
+
+export function getOddsProvider(name = 'theOddsApi') {
+  const provider = providers[name];
+
+  if (!provider) {
+    throw new HttpError(500, `Unknown odds provider "${name}"`);
+  }
+
+  if (!isConfigured(name)) {
+    throw new HttpError(503, `Odds provider "${name}" is not configured`);
+  }
+
+  return provider;
 }
