61ca4363486ced2babe30218a02bb2a03c1c0197
Jacek444 <Jakgreenbay@gmail.com>
2026-09-22 17:48:56 -0500

    route scores through provider layer

--- src/routes/oddsApi.js
+++ src/routes/oddsApi.js
@@ -104,11 +104,14 @@ router.get('/:sport/odds', async (req, res, next) => {
 
 router.get('/:sport/scores', async (req, res, next) => {
   try {
-    const result = await callOddsApi(`/v4/sports/${req.params.sport}/scores`, {
+    const provider = getOddsProvider();
+
+    const result = await provider.getScores(req.params.sport, {
       daysFrom: req.query.daysFrom,
       dateFormat: req.query.dateFormat || 'iso',
       eventIds: req.query.eventIds
     });
+
     res.json(result);
   } catch (error) {
     next(error);
