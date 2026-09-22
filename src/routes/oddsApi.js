d0373abff6c13c0ab041ee1b75c4e02eff3fad04
Jacek444 <Jakgreenbay@gmail.com>
2026-09-22 18:47:14 -0500

    Update oddsApi.js

--- src/routes/oddsApi.js
+++ src/routes/oddsApi.js
@@ -12,7 +12,7 @@ function requireOddsApiKey() {
 
 router.get('/sports', async (req, res, next) => {
   try {
-    const provider = getOddsProvider();
+    const provider = getOddsProvider('sportsGameOdds');
 
     const result = await provider.getSports({
       all: req.query.all
